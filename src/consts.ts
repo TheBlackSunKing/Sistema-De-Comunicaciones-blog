8b468ba58388d47ccd2149454f4d805b5655e634
TheBlackSunKing <97323767+TheBlackSunKing@users.noreply.github.com>
2024-02-07 14:36:48 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Blog de sistema de comunicaciones';
+export const SITE_DESCRIPTION = 'Sitio web hecho por estudiantes de la Universidad Nacional Experimental de Guayana con el proposito de difundir conocimientos sobre los que son los sistemas de comunicaciones!';
